133bbefb4d1c656ae8b2f91eb60dae9d1de6b3de
Kasper Anon <kasperthornsten@gmail.com>
2025-02-16 11:28:05 +0100

    Implement basic echo server with tokio async

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let listener = TcpListener::bind("127.0.0.1:12100").await?;
+
+    loop {
+        let (mut socket, _address) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            let mut buffer = [0; 1024];
+
+            loop {
+                let data = match socket.read(&mut buffer).await {
+                    Ok(0) => return,
+                    Ok(data) => data,
+                    Err(errno) => {
+                        eprintln!("[-] Failed to read from socket; Error = {:?} . . .", errno);
+                        return;
+                    }
+                };
+
+                eprintln!("[+] Data received {} . . .", std::str::from_utf8(&buffer).unwrap());
+
+                if let Err(errno) = socket.write_all(&buffer[0..data]).await {
+                    eprintln!("[-] Failed to write to socket; Error = {:?} . . .", errno);
+                }
+            }
+        });
+    }
 }
